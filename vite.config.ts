93c67099a63267b154d6a2499b415a723a30ef63
Tim Lindborg <timl@live.se>
2024-05-27 10:10:24 +0200

    config mime

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import mime from 'mime';
-import type { Connect } from 'vite';
-
-// Define MIME types for TypeScript files
-const setMimeType: Connect.NextHandleFunction = (req, res, next) => {
-  if (req.url?.endsWith('.ts')) {
-    res.setHeader('Content-Type', mime.getType('ts') || 'application/javascript');
-  }
-  if (req.url?.endsWith('.tsx')) {
-    res.setHeader('Content-Type', mime.getType('tsx') || 'application/javascript');
-  }
-  next();
-};
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'configure-server',
-      configureServer(server) {
-        server.middlewares.use(setMimeType);
-      }
-    }
-  ]
+  plugins: [react(), tsconfigPaths()],
 });
