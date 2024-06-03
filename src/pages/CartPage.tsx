59ae821c3c8df82af617c1ae53ba23688f69a1e2
Tim Lindborg <timl@live.se>
2024-06-03 09:26:34 +0200

    centerfull

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -38,47 +38,48 @@ const CartPage: React.FC = () => {
     };
 
     return (
-        <div className='cart-page'>
-            <div className='cart-container'>
-                <h1>Cart</h1>
-                {/* Render Cart and checkout form based on visibility state */}
-                {isCartPageVisible && (                 
-                    <Cart />
-                )}
-                
-            </div>
-            
-            <div className='form-container'>
-            
-                {isFormVisible && (
-                    <form onSubmit={handleSubmit}>
-                        <div className='input-text'>
-                            <input type="text" name="fullName" placeholder="Full Name" value={formData.fullName} onChange={handleChange} required />
-                            <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} required />
-                            <input type="text" name="address" placeholder="Address" value={formData.address} onChange={handleChange} required />
-                            <input type="text" name="city" placeholder="City" value={formData.city} onChange={handleChange} required />
-                            <input type="text" name="postalCode" placeholder="Postal Code" value={formData.postalCode} onChange={handleChange} required />
-                        </div>
-                        <p className='total-price'>Total Price: ${totalPrice.toFixed(2)}</p>
-                        <button type="submit" className='checkout_btn'>Checkout</button>
-                        
-                    </form>
-                )}
-
-                {/* Button to toggle form visibility */}
-                <div className='hide-btn'>
-                    {!isFormVisible && (
-                        <button onClick={toggleFormVisibility} className="hideform_btn">Go To Checkout</button>
+        <div className='cart-page-container'>
+            <div className='cart-page'>
+                <div className='cart-container'>
+                    <h1>Cart</h1>
+                    {/* Render Cart and checkout form based on visibility state */}
+                    {isCartPageVisible && (                 
+                        <Cart />
                     )}
                 </div>
-                {/* Button to navigate back to homepage */}
-                <Link to="/" className="cartexit_btn">X</Link>
+                
+                <div className='form-container'>
+                
+                    {isFormVisible && (
+                        <form onSubmit={handleSubmit}>
+                            <div className='input-text'>
+                                <input type="text" name="fullName" placeholder="Full Name" value={formData.fullName} onChange={handleChange} required />
+                                <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} required />
+                                <input type="text" name="address" placeholder="Address" value={formData.address} onChange={handleChange} required />
+                                <input type="text" name="city" placeholder="City" value={formData.city} onChange={handleChange} required />
+                                <input type="text" name="postalCode" placeholder="Postal Code" value={formData.postalCode} onChange={handleChange} required />
+                            </div>
+                            <p className='total-price'>Total Price: ${totalPrice.toFixed(2)}</p>
+                            <button type="submit" className='checkout_btn'>Checkout</button>
+                        </form>
+                    )}
+
+                    {/* Button to toggle form visibility */}
+                    <div className='hide-btn'>
+                        {!isFormVisible && (
+                            <button onClick={toggleFormVisibility} className="hideform_btn">Go To Checkout</button>
+                        )}
+                    </div>
+                    {/* Button to navigate back to homepage */}
+                    <Link to="/" className="cartexit_btn">X</Link>
+                </div>
             </div>
         </div>
     );
 };
 
 export default CartPage;
+
 
 
 
