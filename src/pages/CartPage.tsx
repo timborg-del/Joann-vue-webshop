647a1daaa9787db4db07390c41b93b6c35445498
Tim Lindborg <timl@live.se>
2024-06-23 11:51:07 +0200

    .

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Cart from '../components/Cart';
 import PaypalStuff from '../components/PaypalStuff';
 import './CartPage.css';
@@ -10,31 +10,8 @@ interface CartPageProps {
 }
 
 const CartPage: React.FC<CartPageProps> = ({ isVisible, onClose }) => {
-    const [formData, setFormData] = useState({
-        fullName: '',
-        email: '',
-        address: '',
-        city: '',
-        postalCode: '',
-        consent: false,
-    });
     const { state } = useCart();
-
     const totalPrice = state.items.reduce((total, item) => total + item.Price * item.quantity, 0);
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value, type, checked } = e.target;
-        setFormData({ ...formData, [name]: type === 'checkbox' ? checked : value });
-    };
-
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (formData.consent) {
-            console.log(formData);
-        } else {
-            alert("Please provide consent to proceed.");
-        }
-    };
 
     return (
         <div className={`cart-page-container ${isVisible ? 'active' : ''}`}>
@@ -45,62 +22,8 @@ const CartPage: React.FC<CartPageProps> = ({ isVisible, onClose }) => {
                         <Cart />
                     </div>
                     <div className='form-container'>
-                        <form onSubmit={handleSubmit}>
-                            <div className='input-text'>
-                                <input 
-                                    type="text" 
-                                    name="fullName" 
-                                    placeholder="Full Name" 
-                                    value={formData.fullName} 
-                                    onChange={handleChange} 
-                                    required 
-                                />
-                                <input 
-                                    type="email" 
-                                    name="email" 
-                                    placeholder="Email" 
-                                    value={formData.email} 
-                                    onChange={handleChange} 
-                                    required 
-                                />
-                                <input 
-                                    type="text" 
-                                    name="address" 
-                                    placeholder="Address" 
-                                    value={formData.address} 
-                                    onChange={handleChange} 
-                                    required 
-                                />
-                                <input 
-                                    type="text" 
-                                    name="city" 
-                                    placeholder="City" 
-                                    value={formData.city} 
-                                    onChange={handleChange} 
-                                    required 
-                                />
-                                <input 
-                                    type="text" 
-                                    name="postalCode" 
-                                    placeholder="Postal Code" 
-                                    value={formData.postalCode} 
-                                    onChange={handleChange} 
-                                    required 
-                                />
-                            </div>
-                            <div>
-                                <input 
-                                    type="checkbox" 
-                                    name="consent" 
-                                    checked={formData.consent} 
-                                    onChange={handleChange} 
-                                    required 
-                                />
-                                <label htmlFor="consent">I consent to the collection and use of my personal data as described in the privacy policy.</label>
-                            </div>
-                            <p className='total-price'>Total Price: ${totalPrice.toFixed(2)}</p>
-                            <PaypalStuff cart={state.items} />
-                        </form>
+                        <p className='total-price'>Total Price: ${totalPrice.toFixed(2)}</p>
+                        <PaypalStuff cart={state.items} />
                     </div>
                 </div>
             </div>
@@ -109,7 +32,6 @@ const CartPage: React.FC<CartPageProps> = ({ isVisible, onClose }) => {
 };
 
 export default CartPage;
-
 
 
 
