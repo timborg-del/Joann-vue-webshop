07cc31f589bf2233c7888ba1c3abf180ca0a42a6
Tim Lindborg <timl@live.se>
2024-07-02 11:59:12 +0200

    .

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,6 @@ import Showroom from '../components/Showroom'; // Import the Showroom component
 
 const Home: React.FC = () => {
   const [isVisible, setIsVisible] = useState(false);
-  const [isTransitioning, setIsTransitioning] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -14,23 +13,20 @@ const Home: React.FC = () => {
   }, []);
 
   const handleImageClick = (productName: string) => {
-    setIsTransitioning(true);
-    setTimeout(() => {
-      navigate(`/shoppage?product=${productName}`);
-    }, 500); // Match the duration of the opacity transition
+    navigate(`/shoppage?product=${productName}`);
   };
 
   return (
-    <div className="home-container">
+    <div className={`home-container ${isVisible ? 'visible' : ''}`}>
       <h2>Welcome</h2>
-      <div className={`home-content ${isVisible ? 'visible' : ''}`}>
+      <div className="home-content">
         <div className="intro-text">
           <h1>Welcome to Jo's Art Shop</h1>
           <p>
             Hey there! We're glad you stopped by. At Jo's Art Shop, you'll find a variety of unique and beautiful artworks. Whether you're looking for something to brighten up your home or a special gift for someone, we've got you covered. Take a look around and find something you love!
           </p>
         </div>
-        <div className={`image-section ${isTransitioning ? 'fade-out' : ''}`}>
+        <div className="image-section">
           <Showroom onImageClick={handleImageClick} /> {/* Add the Showroom component here */}
         </div>
       </div>
@@ -39,6 +35,7 @@ const Home: React.FC = () => {
 };
 
 export default Home;
+
 
 
 
