3c3d4da40772ed11b90dca37569e794e10ad4c9a
timborg-del <timl@live.se>
2024-07-10 10:27:56 +0200

    hamburger

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,19 @@ import ChangePassword from './components/ChangePassword';
 import PrivateRoute from './components/PrivateRoute';
 import CartPage from './pages/CartPage';
 import { CurrencyProvider } from './components/CurrencyDetector';
-import ContactMe from './pages/ContactMe'; // Make sure to import ContactMe
+import ContactMe from './pages/ContactMe'; // Ensure you have this import
 
 const App: React.FC = () => {
   const [isShopOpen, setIsShopOpen] = useState(false);
   const [isCartVisible, setIsCartVisible] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleCartVisibility = () => {
     setIsCartVisible(!isCartVisible);
+  };
+
+  const toggleMenuVisibility = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
   return (
@@ -27,19 +32,29 @@ const App: React.FC = () => {
       <CurrencyProvider>
         <CartProvider>
           <nav className="navbar">
-            <div className='centerLinks'>        
-              <Link to="/">Home</Link>
-              <Link to="/about">About</Link>            
-              <Link to="/contact" className="contact-link">Contact</Link>
-              <div className="dropdown" onMouseEnter={() => setIsShopOpen(true)} onMouseLeave={() => setIsShopOpen(false)}>           
-                <h1 className='h1shop'>Shop</h1>
-                <div className={`dropdownContent ${isShopOpen ? 'open' : ''}`}>
-                  <Link to="/shoppage">Art-Prints</Link>
+            <div className='centerLinks'>
+              <button className={`hamburger ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenuVisibility}>
+                <span className="hamburger-bar"></span>
+                <span className="hamburger-bar"></span>
+                <span className="hamburger-bar"></span>
+              </button>
+              <div className={`menu ${isMenuOpen ? 'open' : ''}`}>
+                <Link to="/" onClick={() => setIsMenuOpen(false)}>Home</Link>
+                <Link to="/about" onClick={() => setIsMenuOpen(false)}>About</Link>
+                <Link to="/contact" className="contact-link" onClick={() => setIsMenuOpen(false)}>Contact</Link>
+                <div 
+                  className="dropdown" 
+                  onMouseEnter={() => setIsShopOpen(true)} 
+                  onMouseLeave={() => setIsShopOpen(false)}
+                >
+                  <h1 className='h1shop'>Shop</h1>
+                  <div className={`dropdownContent ${isShopOpen ? 'open' : ''}`}>
+                    <Link to="/shoppage" onClick={() => setIsMenuOpen(false)}>Art-Prints</Link>
+                  </div>
+                  <Link to="/admin"></Link>
                 </div>
-                <Link to="/admin"></Link>
               </div>
-              
-            </div>            
+            </div>
             <div>
               <button className="cart-button" onClick={toggleCartVisibility}>
                 <CartButton onClick={toggleCartVisibility} />
